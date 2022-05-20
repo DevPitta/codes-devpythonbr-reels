7153e4cbd673bdc4ad5577158a437f38c1091100
Pitta <arthur_pitta@yahoo.com.br>
2022-05-20 09:33:47 -0300

    Internet speedtest code

---
+++ source/internet_speedtest.py
@@ -0,0 +1,13 @@
+import speedtest
+import webbrowser
+
+speed = speedtest.Speedtest()
+speed.get_servers()
+speed.get_best_server()
+speed.download()
+speed.upload()
+speed.results.share()
+
+results = speed.results.dict()
+print(results)
+webbrowser.open_new(results['share'])
